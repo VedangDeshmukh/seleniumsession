89ee4a9ee031dec4c807dd8bcd2f5b25d7c421d1
VedangDeshmukh <deshmukhvedang7@gmail.com>
2022-06-26 22:21:59 +0530

    added the cart feature

---
+++ src/main/java/Selenium/CartPage.java
@@ -0,0 +1,13 @@
+package Selenium;
+
+public class CartPage {
+
+	
+	public void addToCart() {
+		System.out.println("Add to cart feature is added");
+	}
+
+	public void check() {
+		System.out.println("checkadd");
+	}
+}
---
+++ src/test/java/TestNgBasic/CartTest.java
@@ -0,0 +1,12 @@
+package TestNgBasic;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	
+	@Test
+	public void CartTest() {
+		System.out.println("Cart fetaure Test");
+	}
+}
